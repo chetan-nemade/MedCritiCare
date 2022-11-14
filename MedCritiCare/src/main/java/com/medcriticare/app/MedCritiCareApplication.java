942c384cd68e7d64c64a6a6cc665222295881022
Chetan Nemade <chetan.nemade@gmail.com>
2022-11-14 17:35:07 +0530

    First Commit

---
+++ MedCritiCare/src/main/java/com/medcriticare/app/MedCritiCareApplication.java
@@ -0,0 +1,13 @@
+package com.medcriticare.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCritiCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedCritiCareApplication.class, args);
+	}
+
+}
---
+++ MedCritiCare/src/test/java/com/medcriticare/app/MedCritiCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medcriticare.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCritiCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
